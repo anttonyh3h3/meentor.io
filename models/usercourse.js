e1d07cafd72dd49ba07945345a53e30046757fc2
YasserAlifanno <fannohashfive@gmail.com>
2022-12-08 12:03:28 +0700

    model create

---
+++ models/usercourse.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserCourse extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      UserCourse.belongsToMany(models.User)
+      UserCourse.belongsToMany(models.Course)
+
+    }
+  }
+  UserCourse.init({
+    UserId: DataTypes.INTEGER,
+    CourseId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'UserCourse',
+  });
+  return UserCourse;
+};
